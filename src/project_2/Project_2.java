018591f1472a965de236dfdaa72781c20c451a13
SupraMensch <cwadeburt@gmail.com>
2018-10-10 21:11:47 -0500

    Instructions and name

---
+++ src/project_2/Project_2.java
@@ -0,0 +1,27 @@
+//  Christopher Burt
+
+//  Project_2
+
+//
+//
+//  Sort an Array (20 points)
+//
+//  Create an array of integers that contains the following:
+//
+//  int[] numbers = { 12, 193, 56, 74, 9, 34, 165, 102, 154, 63, 19, 49 }
+//
+//  In your main program, sort the array and display all of the elements in sorted order.
+
+
+
+package project_2;
+
+
+public class Project_2 {
+
+    public static void main(String[] args)
+    {
+        // TODO code application logic here
+    }
+    
+}
